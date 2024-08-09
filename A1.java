2d561050b6e3cb955470441ec05a4c0a4f2fc438
nicholassteuart <c3330826@uon.edu.au>
2024-08-09 21:28:21 +1000

    Comment Template commit

--- A1.java
+++ A1.java
@@ -1,3 +1,10 @@
+/* COMP2240 Assignment 1
+ * File: A1.java
+ * Author: Nicholas Steuart c3330826
+ * Date Created: 9/8/24
+ * Date Last Modified: 9/8/24
+ * Description: MAIN file. Takes File I/O data and calculates Scheduling Algorithm simulations.
+ */
 public class A1
 {
     public static void main(String[] args)
